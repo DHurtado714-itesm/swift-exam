aee7008e7a6f047266e9c3df9f3840c55d7ecbca
Daniel Hurtado <a01707774@tec.mx>
2023-10-10 11:13:03 -0600

    feat(swift): initial commit

---
+++ swfit-examan/swfit-examan/swfit_examanApp.swift
@@ -0,0 +1,17 @@
+//
+//  swfit_examanApp.swift
+//  swfit-examan
+//
+//  Created by Daniel Hurtado on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct swfit_examanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
