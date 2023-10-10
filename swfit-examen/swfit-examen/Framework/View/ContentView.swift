6758b0ff7b0b6a0de15ac2b18bf47caf125fee78
Daniel Hurtado <a01707774@tec.mx>
2023-10-10 11:45:06 -0600

    feat(view): develop basic layout

--- swfit-examen/swfit-examen/Framework/View/ContentView.swift
+++ swfit-examen/swfit-examen/Framework/View/ContentView.swift
@@ -1,21 +1,58 @@
-//
-//  ContentView.swift
-//  swfit-examen
-//
-//  Created by Daniel Hurtado on 10/10/23.
-//
-
 import SwiftUI
+import SDWebImageSwiftUI
+
+struct MovieRow: View {
+    // Datos de muestra
+    var movie = (
+        title: "The Nun II",
+        releaseDate: "2023-09-06",
+        overview: "In 1956 France, a priest is violently murdered...",
+        voteAverage: 7.0,
+        voteCount: 776,
+        posterURL: URL(string: "https://image.tmdb.org/t/p/original/5gzzkR7y3hnY8AD1wXjCnVlHba5.jpg")
+    )
+    
+    var body: some View {
+        HStack {
+            // Imagen
+            if let url = movie.posterURL {
+                WebImage(url: movie.posterURL)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 150)
+                    .clipped()
+            }
+            
+            // Detalles
+            VStack(alignment: .leading) {
+                Text(movie.title)
+                    .font(.headline)
+                Text(movie.releaseDate)
+                    .font(.subheadline)
+                Text(movie.overview)
+                    .font(.caption)
+                    .lineLimit(3)
+                    .truncationMode(.tail)
+                
+                HStack {
+                    Text("Promedio: \(movie.voteAverage, specifier: "%.1f")")
+                    Spacer()
+                    Text("Votos: \(movie.voteCount)")
+                }
+                .font(.caption)
+            }
+            .padding(.leading, 5)
+        }
+    }
+}
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world examen!")
+            List {
+                MovieRow() // Solo mostrando una fila de muestra
+            }
         }
-        .padding()
     }
 }
 
